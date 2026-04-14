05d6d48ff347f46857ab1cbeea1d86fa6fe792ef
Cheyenne Gowie <yenniegowie6@gmail.com>
2026-04-13 22:42:55 -0500

    Created and completed Add movie form

---
+++ src/views/AddMovieFormView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mt-4">
+    <h2>Upload Form</h2>
+    <MovieForm />
+  </div>
+</template>
+
+<script setup>
+import MovieForm from '@/components/MovieForm.vue';
+</script>
+
+<style scoped>
+</style>
