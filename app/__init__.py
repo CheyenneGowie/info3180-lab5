d4f321eb485e70368ad70c8396339b19ca895428
Cheyenne Gowie <yenniegowie6@gmail.com>
2026-04-13 22:33:12 -0500

    Modified init,py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+import os
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'app/uploads')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Info3180@localhost/lab5"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
 
 from app import views
